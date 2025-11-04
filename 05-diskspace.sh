781d53962599b175f6f811f29b8ced5169bb8603
AvinDevops <avinashbandela73@gmail.com>
2025-11-04 17:00:33 +0530

    disk threshold

--- 05-diskspace.sh
+++ 05-diskspace.sh
@@ -7,6 +7,7 @@ while IFS= read -r line
 do
     USAGE=$($DISKSOURCE | awk -F " " '{print $6F}' | cut -d "%" -f1)
     FOLDER=$($DISKSOURCE | awk -F " " '{print $NF}')
+    echo "$USAGE and $FOLDER"
     if [ $USAGE -ge $DISKTHRESHOLD ]
     then
         echo "this folder $FOLDER and $USAGE usage is more than threshold $DISKTHRESHOLD"
