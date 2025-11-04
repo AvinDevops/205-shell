9e6ec54dfac520a6e61929c197501413b32deda4
AvinDevops <avinashbandela73@gmail.com>
2025-11-04 16:11:55 +0530

    installing with loops

--- 03-installing-apps.sh
+++ 03-installing-apps.sh
@@ -14,6 +14,16 @@ else
     echo -e "$G you are root user $N"
 fi 
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$R $2 is...FAILED $N"
+        exit 1
+    else
+        echo -e "$G $2 is...SUCCESS $N"
+    fi
+}
+
 for i in $@
 do
     echo "installing $i"
@@ -21,9 +31,9 @@ do
     if [ $? -eq 0 ]
     then
         echo -e "$G $i already installed $N"
-        exit 1
     else
         dnf install $i -y
+        VALIDATE $? "installing $i"
     fi
 done
 
