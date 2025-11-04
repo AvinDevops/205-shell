bbddcb1fcb7f742d1ef198a0cb79e753c18a5fc6
AvinDevops <avinashbandela73@gmail.com>
2025-11-04 16:57:12 +0530

    disk threshold

--- 05-diskspace.sh
+++ 05-diskspace.sh
@@ -7,7 +7,7 @@ while IFS= read -r line
 do
     CURRUSAGE=$($DISKSOURCE | awk -F " " '{print $6F}' | cut -d "%" -f1)
     FOLDER=$($DISKSOURCE | awk -F " " '{print $NF}')
-    if [ $DISKTHRESHOLD -gt $CURRUSAGE ]
+    if [ $CURRUSAGE -gt $DISKTHRESHOLD ]
     then
         echo "this folder $FOLDER and $CURRUSAGE usage is more than threshold $DISKTHRESHOLD"
     fi
