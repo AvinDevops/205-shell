16cb7701e0dcf01d6b1ee084b88dc89eaaeb7df4
AvinDevops <avinashbandela73@gmail.com>
2025-11-04 17:32:19 +0530

    current script

--- 06-current-script.sh
+++ 06-current-script.sh
@@ -4,6 +4,7 @@ COURSE="DevOps with avin"
 echo "current script course value is: $COURSE"
 echo "current script PID: $$"
 
-./07-other-script.sh
+# ./07-other-script.sh
+source ./07-other-script.sh
 
 echo "after calling other script course is: $COURSE"
