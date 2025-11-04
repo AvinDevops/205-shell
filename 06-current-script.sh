9da500b2b9a13271d3750ddbc42f3657fc046085
AvinDevops <avinashbandela73@gmail.com>
2025-11-04 17:17:03 +0530

    current and other script

---
+++ 06-current-script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+COURSE=DevOps with avin
+
+echo "current script course value is: $COURSE"
+echo "current script PID: $$"
+
+./07-other-script.sh
---
+++ 07-other-script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+COURSE=Kubernetes with kumar
+
+echo "other script course value is: $COURSE"
+echo "other script PID: $$"
