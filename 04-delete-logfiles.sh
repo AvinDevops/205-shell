5d9302bba2298d1ec093278709d61057f9b6f7ae
AvinDevops <avinashbandela73@gmail.com>
2025-11-04 16:28:29 +0530

    deleteing logs

---
+++ 04-delete-logfiles.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SOURCEDIR=/home/ec2-user/logs
+
+if [ -d $SOURCEDIR ]
+then
+    echo "source dir is existed."
+    exit 1
+else    
+    echo "source dir is not there."
+fi
+
+FILE=$(find $SOURCEDIR -name "*.log" -mtime +14)
+
+while IFS= read -r line
+do
+    echo "$line need to be deleted"
+done << $FILE
